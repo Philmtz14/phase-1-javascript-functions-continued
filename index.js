7225f5c4b4d2f2f22d2fa744c7c638ee10178317
philip martinez <martinez.philip00@gmail.com>
2022-03-09 00:53:06 -0600

    commit tree 549520b85b3beed43ec6cdaa0a729306d882ba0f

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // code your solution here
+function saturdayFun (noActivity, activity) {
+    switch (noActivity, activity) {
+        case noActivity:
+            return 'This Saturday, I want to roller-skate!';
+        case activity:
+            return 'This Saturday, I want to bathe my dog!';
+    }
+}
+
+function mondayWork (activity, overriden) {
+    switch (activity, overriden){
+        case activity:
+            return 'This Monday, I will go to the office.';
+        case overriden:
+            return 'This Monday, I will work from home.';
+    }
+}
+
+
+function wrapAdjective(result = '*') {
+    return function (blank = 'special') {
+        return 'You are ' + `${result}` + `${blank}` + `${result}` + '!';
+    } 
+}
+wrapAdjective();
